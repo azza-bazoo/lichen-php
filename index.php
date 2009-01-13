8a46fd8a3a04594e7c6736638d3d8bff30153a0d
Hourann Bosci <hourannb@dview.net>
2009-01-13 09:27:31 +0000

    [JS interface] change - switch back to non-minified js for testing
    git-svn-id: file:///home/dabosq/lichen/svn/trunk@269 bb28ca92-9e36-0410-91a4-d1cafbab33cc

--- index.php
+++ index.php
@@ -137,7 +137,22 @@ if ( ( isset( $_POST['user'] ) && !empty( $_POST['user'] ) &&
 
 		echo <<<ENDJS
 <script type="text/javascript" src="js/mootools.v1.11.js"></script>
-<script type="text/javascript" src="js/lichen-r249.js"></script>
+<script type="text/javascript" src="js/tinymce/tiny_mce.js"></script>
+<script type="text/javascript" src="js/initialisation.js"></script>
+<script type="text/javascript" src="js/interface-feedback.js"></script>
+<script type="text/javascript" src="js/interface-helpers.js"></script>
+<script type="text/javascript" src="js/mailbox-manager.js"></script>
+<script type="text/javascript" src="js/mailboxes-list.js"></script>
+<script type="text/javascript" src="js/mailbox-data.js"></script>
+<script type="text/javascript" src="js/cache.js"></script>
+<script type="text/javascript" src="js/server-connector.js"></script>
+<script type="text/javascript" src="js/message-listings.js"></script>
+<script type="text/javascript" src="js/message-display.js"></script>
+<script type="text/javascript" src="js/composer.js"></script>
+<script type="text/javascript" src="js/settings.js"></script>
+<script type="text/javascript" src="js/addressbook.js"></script>
+<script type="text/javascript" src="js/interface-controller.js"></script>
+<script type="text/javascript" src="js/html-formatting.js"></script>
 ENDJS;
 
 		drawToolbar( 'corner-bar' );
